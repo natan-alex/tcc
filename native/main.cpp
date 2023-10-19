470bd34436b8c2ee9add00877626625c3148b67b
natan-alex <natancarmo321@gmail.com>
2023-10-18 22:54:37 -0300

    remove unnecessary comments

--- native/main.cpp
+++ native/main.cpp
@@ -8,12 +8,10 @@
 #include "opencv2/imgcodecs/imgcodecs.hpp"
 
 int main() {
-    // Load the images
     cv::Mat source_image = cv::imread("../assets/nature.jpg");
     cv::Mat template_image = cv::imread("../assets/nature-tree.png");
 
 
-    // Initialize variables
     cv::Mat mask;
     cv::Mat result;
     cv::Point minLoc, maxLoc;
@@ -21,7 +19,6 @@ int main() {
 
     auto start = std::chrono::steady_clock::now();
 
-    // Perform template matching using NCC
     cv::matchTemplate(source_image, template_image, result, cv::TM_CCOEFF_NORMED, mask);
 
     auto end = std::chrono::steady_clock::now();
@@ -31,7 +28,6 @@ int main() {
     cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc);
 
 
-    // Draw a rectangle around the best match
     cv::Point rect_bottom_right(
         maxLoc.x + template_image.cols,
         maxLoc.y + template_image.rows
@@ -42,7 +38,6 @@ int main() {
     cv::rectangle(source_image, maxLoc, rect_bottom_right, rect_color, 2);
 
 
-    // Display the result
     cv::imshow("Result", source_image);
     cv::waitKey(0);
 
