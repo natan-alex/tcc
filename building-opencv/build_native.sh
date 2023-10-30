61ff38d3e3be2a1f96f4323ea950db30bd11ce3a
natan-alex <natancarmo321@gmail.com>
2023-10-29 22:40:53 -0300

    add flags to disable parallel processing

--- building-opencv/build_native.sh
+++ building-opencv/build_native.sh
@@ -3,7 +3,7 @@ git clone --depth 1 https://github.com/opencv/opencv.git opencv
 (
     mkdir -p build_native && cd build_native &&
 
-    cmake -DCMAKE_INSTALL_PREFIX=/home/natan/libs/opencv4.8 ../opencv
+    cmake -DWITH_IPP=OFF -DWITH_TBB=OFF -DWITH_OPENMP=OFF -DWITH_PTHREADS_PF=OFF -DCMAKE_INSTALL_PREFIX=/home/natan/libs/opencv4.8 ../opencv
 
     cmake --build . --target install
 )
